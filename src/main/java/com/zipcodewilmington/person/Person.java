93c3cd42c21c586e0b637b723d3620342a6fbc5b
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-03-05 11:24:38 -0500

    lab complete

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,98 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String city;
+    private String state;
+    private String phoneNumber;
+    private String emailAddress;
+    private int lastFourDigitsSSN;
 
-    public Person() {
+    public Person() { // https://stackoverflow.com/questions/31284566/how-to-create-instance-of-a-object-without-passing-argument-in-constructor
+    name = "";
+    age = Integer.MAX_VALUE;
+    city = "Miami";
+    state = "Florida";
+    phoneNumber = "234-567-8901";
+    emailAddress = "beepboopbop@thegovernment.gov";
+    lastFourDigitsSSN = 1092;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, String city, String state, String phoneNumber, String emailAddress, int lastFourDigitsSSN) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.lastFourDigitsSSN = lastFourDigitsSSN;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setLastFourDigitsSSN(int lastFourDigitsSSN) {
+        this.lastFourDigitsSSN = lastFourDigitsSSN;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public int getLastFourDigitsSSN() {
+        return lastFourDigitsSSN;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -25,6 +25,36 @@ public class TestPerson {
     }
 
     @Test
+    public void testDefaultConstructor2() {
+        // Given
+        String expectedName = "";
+        Integer expectedAge = Integer.MAX_VALUE;
+        String expectedCity = "Miami";
+        String expectedState = "Florida";
+        String expectedPhoneNumber = "234-567-8901";
+        String expectedEmailAddress = "beepboopbop@thegovernment.gov";
+        Integer expectedLastFourDigitsSSN = 1092;
+        // When
+        Person person = new Person();
+        // Then
+        String actualName = person.getName();
+        Integer actualAge = person.getAge();
+        String actualCity = person.getCity();
+        String actualState = person.getState();
+        String actualPhoneNumber = person.getPhoneNumber();
+        String actualEmailAddress = person.getEmailAddress();
+        Integer actualLastFourDigitsSSN = person.getLastFourDigitsSSN();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedCity, actualCity);
+        Assert.assertEquals(expectedState, actualState);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        Assert.assertEquals(expectedEmailAddress, actualEmailAddress);
+        Assert.assertEquals(expectedLastFourDigitsSSN, actualLastFourDigitsSSN);
+    }
+
+    @Test
     public void testConstructorWithName() {
         // Given
         String expected = "Leon";
@@ -93,6 +123,66 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetCity() {
+        // Given
+        Person person = new Person();
+        String expected = "Wilmington";
+        // When
+        person.setCity(expected);
+        // Then
+        String actual = person.getCity();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetState() {
+        // Given
+        Person person = new Person();
+        String expected = "Delaware";
+        // When
+        person.setState(expected);
+        // Then
+        String actual = person.getState();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "123-456-7890";
+        // When
+        person.setPhoneNumber(expected);
+        // Then
+        String actual = person.getPhoneNumber();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEmailAddress() {
+        // Given
+        Person person = new Person();
+        String expected = "humanperson@ZCW.interwebz";
+        // When
+        person.setEmailAddress(expected);
+        // Then
+        String actual = person.getEmailAddress();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLastFourDigitsSSN() {
+        // Given
+        Person person = new Person();
+        Integer expected = 1234;
+        // When
+        person.setLastFourDigitsSSN(expected);
+        // Then
+        Integer actual = person.getLastFourDigitsSSN();
         Assert.assertEquals(expected, actual);
     }
 }
